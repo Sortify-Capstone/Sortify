aa4010765d99811c31b8d3bce88a0e68a6bbb45f
Fahrul Setiawan <119778876+fahrulsn@users.noreply.github.com>
2023-06-16 14:03:12 +0700

    Update code server.js

--- Cloud Computing/sortify-api/server.js
+++ Cloud Computing/sortify-api/server.js
@@ -2,19 +2,36 @@ const Hapi = require("@hapi/hapi");
 const { Storage } = require("@google-cloud/storage");
 const axios = require("axios");
 const sharp = require("sharp");
-const fs = require("fs");
 const { nanoid } = require("nanoid");
+const admin = require("firebase-admin");
+const { classification } = require("./classes");
 
-const projectId = "feisty-truth-381413";
-const servingUrl = "";
-const bucketName = "sortify-img";
+const projectId = "capstone-sortify";
+const servingUrl = "https://sortify-model-6x3vm2nioq-et.a.run.app";
+const bucketName = "sortify-app";
 const audioOrganik = "audio/organik-voice.mp3";
 const audioAnorganik = "audio/anorganik-voice.mp3";
+
+// GCS config
 const gcs = new Storage({
   projectId: projectId,
   keyFilename: "servicekey.json",
 });
 
+// firestore config
+const path = require("path");
+const serviceAccount = require(path.join(
+  __dirname,
+  "servicekey-firestore.json"
+));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+// handler
 const uploadImageHandler = async (request, h) => {
   try {
     const id = nanoid(8);
@@ -24,7 +41,7 @@ const uploadImageHandler = async (request, h) => {
     await gcs.bucket(bucketName).upload(imageFile.path, {
       destination: filename,
     });
-    compressImageHandler(bucketName, filename);
+    await compressImageHandler(bucketName, filename);
     const imageUrl = `https://storage.googleapis.com/${bucketName}/${filename}`;
 
     return imageUrl;
@@ -39,7 +56,7 @@ const compressImageHandler = async (bucketName, imageName) => {
     const bucket = gcs.bucket(bucketName);
     const file = bucket.file(imageName);
     const [fileBuffer] = await file.download();
-    const image = sharp(fileBuffer).jpeg({ quality: 30 });
+    const image = sharp(fileBuffer).jpeg({ quality: 70 });
 
     const compressedImageBuffer = await image.toBuffer();
     await file.save(compressedImageBuffer, {
@@ -53,6 +70,20 @@ const compressImageHandler = async (bucketName, imageName) => {
     console.error("Error compressing image:", error);
   }
 };
+const getHistoryData = async () => {
+  try {
+    const snapshot = await db.collection("data").orderBy("timestamp", "desc").get();
+    const data = [];
+    snapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data() });
+    });
+
+    return { data };
+  } catch (error) {
+    console.error("Error getting data:", error);
+    return { error: "Failed to get data" };
+  }
+};
 
 const predictImgHandler = async (request, h) => {
   try {
@@ -60,40 +91,49 @@ const predictImgHandler = async (request, h) => {
     let descriptions;
     let classResult;
     let audio;
-    const image = request.payload.file;
 
-    const respon = await axios.post(
-      `${servingUrl}/path/to/model:predict`,
-      image
+    const payload = { url: imageUrl };
+    const respon = await axios.post(`${servingUrl}/predict`, payload);
+    const predictions = respon.data.predictions[0];
+    const classIndex = predictions.indexOf(Math.max(...predictions));
+
+    const isOrganic = [classification.CARDBOARD, classification.PAPER].includes(
+      classIndex
     );
 
-    // const respon = request.payload;
-    const predictions = respon.predictions[0];
-    const classIndex = predictions.indexOf(Math.max(...predictions));
-    // const classIndex = 0;
+    const isAnorganic = [
+      classification.GLASS,
+      classification.METAL,
+      classification.PLASTIC,
+    ].includes(classIndex);
 
-    switch (classIndex) {
-      case 0:
-        audio = audioOrganik;
-        classResult = "Organik";
-        descriptions =
-          "Sampah organik adalah jenis sampah yang berasal dari bahan-bahan yang dapat terurai secara alami, seperti sisa makanan, daun, rumput, kulit buah, dan lain sebagainya. Daur ulang sampah organik melibatkan proses pengolahan kembali sisa-sisa organik tersebut menjadi bahan yang berguna. Metode daur ulang sampah organik antara lain meliputi pengomposan dan pembuatan biogas. Daur ulang sampah organik membantu mengurangi volume sampah, mengurangi emisi gas rumah kaca, serta menghasilkan bahan yang dapat digunakan kembali, seperti kompos yang berguna sebagai pupuk organik atau biogas yang dapat digunakan sebagai sumber energi alternatif.";
-        break;
-      case 1:
-        audio = audioAnorganik;
-        classResult = "Anorganik";
-        descriptions =
-          "Sampah anorganik adalah sampah yang tidak dapat terurai secara alami oleh mikroorganisme. Ini termasuk bahan seperti plastik, kaca, logam, dan kertas yang terkontaminasi dengan bahan kimia. Daur ulang sampah anorganik melibatkan proses pemrosesan kembali bahan-bahan ini untuk menghasilkan produk baru atau untuk mengurangi penggunaan bahan mentah baru. Beberapa metode daur ulang sampah anorganik meliputi penghancuran, pemisahan bahan, peleburan, dan proses kimia lainnya. Daur ulang sampah anorganik membantu mengurangi penggunaan sumber daya alam dan mengurangi volume sampah yang berakhir di tempat pembuangan akhir.";
-        break;
+    if (isOrganic) {
+      audio = audioOrganik;
+      classResult = "Organik";
+      descriptions =
+        "Sampah organik adalah jenis sampah yang berasal dari bahan-bahan yang dapat terurai secara alami, seperti sisa makanan, daun, rumput, kulit buah, dan lain sebagainya. Daur ulang sampah organik melibatkan proses pengolahan kembali sisa-sisa organik tersebut menjadi bahan yang berguna. Metode daur ulang sampah organik antara lain meliputi pengomposan dan pembuatan biogas. Daur ulang sampah organik membantu mengurangi volume sampah, mengurangi emisi gas rumah kaca, serta menghasilkan bahan yang dapat digunakan kembali, seperti kompos yang berguna sebagai pupuk organik atau biogas yang dapat digunakan sebagai sumber energi alternatif.";
+    } else if (isAnorganic) {
+      audio = audioAnorganik;
+      classResult = "Anorganik";
+      descriptions =
+        "Sampah anorganik adalah sampah yang tidak dapat terurai secara alami oleh mikroorganisme. Ini termasuk bahan seperti plastik, kaca, logam, dan kertas yang terkontaminasi dengan bahan kimia. Daur ulang sampah anorganik melibatkan proses pemrosesan kembali bahan-bahan ini untuk menghasilkan produk baru atau untuk mengurangi penggunaan bahan mentah baru. Beberapa metode daur ulang sampah anorganik meliputi penghancuran, pemisahan bahan, peleburan, dan proses kimia lainnya. Daur ulang sampah anorganik membantu mengurangi penggunaan sumber daya alam dan mengurangi volume sampah yang berakhir di tempat pembuangan akhir.";
     }
 
+    //simpan hasil ke firestore
+    const currentTime = Date.now();
+    const gmtPlus7Time = new Date(currentTime).toLocaleString("en-US", {
+      timeZone: "Asia/Jakarta",
+    });
+    const data = { classResult, imageUrl, timestamp: gmtPlus7Time };
+    const collection = db.collection("data");
+    await collection.add(data);
     return h.response({ classResult, descriptions, audio, imageUrl }).code(200);
   } catch (error) {
     console.log(error);
     return h.response({ message: "Gagal melakukan klasifikasi" }).code(500);
   }
 };
-
+//server
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
@@ -104,7 +144,7 @@ const init = async () => {
       },
     },
   });
-
+  //routes
   server.route([
     {
       method: "GET",
@@ -118,8 +158,15 @@ const init = async () => {
       },
     },
     {
+      method: "GET",
+      path: "/api/data",
+      options: {
+        handler: getHistoryData,
+      },
+    },
+    {
       method: "POST",
-      path: "/api",
+      path: "/api/classify",
       options: {
         handler: predictImgHandler,
         payload: {
@@ -135,6 +182,7 @@ const init = async () => {
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
+//start server
 init().catch((error) => {
   console.error(error);
   process.exit(1);
